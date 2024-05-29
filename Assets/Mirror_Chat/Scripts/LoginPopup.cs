204008dd97a17f77b315b78ce5fd0cf3d101cbf6
조현승 <danitech171791@gmail.com>
2024-05-29 11:34:40 +0900

    [init] 리소스 초기화

---
+++ Assets/Mirror_Chat/Scripts/LoginPopup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginPopup : MonoBehaviour
+{
+    [SerializeField] private InputField Input_NetworkAdress;
+    [SerializeField] private InputField Input_UserName;
+
+    [SerializeField] private Button Btn_StartAsHostServer;
+    [SerializeField] private Button Btn_StartAsClient;
+
+    [SerializeField] private Text Text_Error;
+
+
+}
